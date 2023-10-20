a6e2f616c18c860bf739b1a426692cf41bac5db4
ChrisZZG <cristiangonzz@gmail.com>
2023-10-20 06:37:35 -0300

    act 01

---
+++ citas.js
@@ -0,0 +1,24 @@
+
+let citas = [
+    {'autor': 'Teemo',
+    'texto': 'La guerra nunca cambia, solo cambian las armas.'    
+    },
+    {'autor': 'Ezreal',
+    'texto': 'Casi nunca se lo que estoy buscando, pero siempre lo encuentro.'    
+    },
+    {'autor': 'Nasus',
+    'texto': 'Palabras equivocadas hacen caer imperios.'    
+    },
+    {'autor': 'Poppy',
+    'texto': 'Si estás esperando a que me rinda, vas a tener que esperar un largo rato.'    
+    },
+    {'autor': 'Warwick',
+    'texto': 'Se necesita un monstruo para matar a otro.'    
+    },
+    {'autor': 'Kled',
+    'texto': 'Nadie nunca te va a dar nada. Debes conseguirlo por tu cuenta.'    
+    },
+    {'autor': 'Ekko',
+    'texto': 'El tiempo va curando todas las heridas, pero algunas las escondemos nosotros mismos.'    
+    },
+];
